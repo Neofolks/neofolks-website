5719a561b5f52f0b34a739c0818f80ccd2f3ed9c
OhmPatil <workpatil10@gmail.com>
2023-03-12 19:27:56 +0530

    Change RSVP modal text color

--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -23,7 +23,7 @@ function RegisterModal() {
         <>
           <div className="fixed top-0 right-0 w-[100vw] h-screen bg-slate-600 bg-opacity-50 flex justify-center items-center">
             {/* Modal content */}
-            <div className="bg-white p-2 rounded-xl min-w-[300px] min-h-[300px] flex flex-col justify-evenly items-center">
+            <div className="bg-white text-black p-2 rounded-xl min-w-[300px] min-h-[300px] flex flex-col justify-evenly items-center">
               <h1 className="font-bold text-xl">Tinkerthon Registration</h1>
               <RsvpForm toggleShowModal={toggleShow} />
               <Button color={"failure"} onClick={toggleShow}>
