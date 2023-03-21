fbdc306e629a034d962773251f5c7f6812d81c8e
Dhruv237 <dhruvshah232002@gmail.com>
2023-03-21 13:12:54 +0530

    Add google maps iframe

--- src/sections/TinkerBottom.jsx
+++ src/sections/TinkerBottom.jsx
@@ -22,6 +22,51 @@ function TinkerBottom() {
 
         {/* TODO Map location stuff */}
 
+        <div style={{display: 'flex', flexDirection: 'column', height: '100vh',left:'0'}}className="earth3dmap-com">
+      <iframe
+        id="iframemap"
+        src="https://maps.google.com/maps?q=Navrachna+University%2CVasna-Bhayli+Main+road%2Cvadodara%2CGujarat%2CIndia&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
+        width="100%"
+        height="300"
+        frameBorder="0"
+        scrolling="no"
+        style={{
+          color: '#333',
+          fontSize: '14px',
+          fontFamily: 'Arial, Helvetica, sans-serif',
+          textAlign: 'left',
+          padding: '10px',
+          marginLeft:'-350px',
+          marginRight:'400px',
+          marginTop:'200px'
+         
+                  
+          
+        }}
+      />
+      <div style={{
+          color: '#333',
+          fontSize: '14px',
+          fontFamily: 'Arial, Helvetica, sans-serif',
+          textAlign: 'right',
+          padding: '10px'
+        }}>
+     <a
+          style={{
+            color: '#333',
+            textDecoration: 'underline',
+            fontSize: '14px',
+            fontFamily: 'Arial, Helvetica, sans-serif',
+            textAlign: 'right'
+          }}
+          href="http://earth3dmap.com/?from=embed"
+          target="_blank"
+        >
+        </a>
+      </div>
+    </div>
+ 
+
 
         {/* Address */}
         <div className='text-white font-medium text-center text-lg'>
