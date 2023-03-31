d55763c24178f37ae7b98fe3041289428e89c07a
OhmPatil <workpatil10@gmail.com>
2023-03-31 17:02:08 +0530

    - Disable rsvp form. - Update social wall layout.

--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -22,8 +22,8 @@ function RegisterModal({buttonText}) {
           <div className="fixed top-0 right-0 w-[100vw] h-screen bg-slate-600 bg-opacity-50 flex justify-center items-center z-20">
             {/* Modal content */}
             <div className="bg-slate-300 shadow-xl text-black p-2 rounded-xl min-w-[300px] min-h-[300px] flex flex-col justify-evenly items-center">
-              <h1 className="font-bold text-2xl font-['familjen_grotesk']">Tinkerthon Registration</h1>
-              <RsvpForm toggleShowModal={toggleShow} />
+              <h1 className="font-bold text-2xl font-['familjen_grotesk'] text-center">Tinkerthon Registrations are closed!</h1>
+              {/* <RsvpForm toggleShowModal={toggleShow} /> */}
               <Button color={"failure"} onClick={toggleShow}>
                 Close
               </Button>
--- src/sections/SocialMedia.jsx
+++ src/sections/SocialMedia.jsx
@@ -4,7 +4,7 @@ import { InstagramEmbed } from "react-social-media-embed";
 function SocialMedia() {
   return (
     <div
-      className="relative h-fit lg:min-h-[750px] w-full text-white flex flex-col items-center gap-10 px-2 section"
+      className="hidden xl:flex relative h-fit lg:min-h-[750px] w-full text-white flex-col items-center gap-10 px-2 section"
       id="about"
     >
       {/* Title */}
@@ -15,24 +15,26 @@ function SocialMedia() {
       </div>
 
       {/* Instagram posts */}
-      <div className="w-full flex justify-center gap-6 flex-wrap items-center">
-        <div>
-          <InstagramEmbed
-            url="https://www.instagram.com/p/Cp-0vIGNZd2/"
-            className="w-[328px] h-[380px] lg:w-[428px] lg:h-[480px]"
-          />
-        </div>
-        <div>
-          <InstagramEmbed
-            url="https://www.instagram.com/p/Cp2zK74t9yH/"
-            className="w-[328px] h-[380px] lg:w-[428px] lg:h-[480px]"
-          />
-        </div>
-        <div>
-          <InstagramEmbed
-            url="https://www.instagram.com/p/Cpsr9zntTCT/"
-            className="w-[328px] h-[380px] lg:w-[428px] lg:h-[480px]"
-          />
+      <div className="w-full flex flex-col justify-center items-center">
+        <div className="flex">
+          <div>
+            <InstagramEmbed
+              url="https://www.instagram.com/p/Cp-0vIGNZd2/"
+              className="aspect-[6/7] min-w-[328px]"
+            />
+          </div>
+          <div>
+            <InstagramEmbed
+              url="https://www.instagram.com/p/Cp2zK74t9yH/"
+              className="aspect-[6/7] min-w-[328px]"
+            />
+          </div>
+          <div>
+            <InstagramEmbed
+              url="https://www.instagram.com/p/Cpsr9zntTCT/"
+              className="aspect-[6/7] min-w-[328px]"
+            />
+          </div>
         </div>
       </div>
     </div>
